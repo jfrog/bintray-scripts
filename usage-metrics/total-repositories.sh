7966c69233e761834618e3f0f2fdaca482a69b0a
Mark Galpin <markg@jfrog.com>
2016-08-23 10:16:26 -0700

    Adding percentage calculation at repo level

--- usage-metrics/total-repositories.sh
+++ usage-metrics/total-repositories.sh
@@ -5,17 +5,22 @@ source bintray.secrets
 curl -u$BINTRAY_USERNAME:$BINTRAY_APIKEY https://api.bintray.com/repos/$BINTRAY_ORG > repos.txt
 
 NUM_REPOS=$(cat repos.txt | jq '. | length')
-
+USAGE_URL="https://api.bintray.com/usage/$BINTRAY_ORG"
+curl -u$BINTRAY_USERNAME:$BINTRAY_APIKEY -X POST "$USAGE_URL" -T timeslice.json > total_usage.txt
+TOTAL_STORAGE=$(cat total_usage.txt | jq -r '.[0].storage_bytes')
+TOTAL_DL=$(cat total_usage.txt | jq -r '.[0].download_bytes')
 echo $NUM_REPOS
-echo "repository, storage, downloads" > usage.csv
+echo 'repository, storage(b), downloaded(b), storage %, downloaded %' > usage.csv
 COUNTER=0
 while [ $COUNTER -lt $NUM_REPOS ]; do
     USAGE_URL="https://api.bintray.com/usage/$BINTRAY_ORG/$(cat repos.txt | jq -r ".[$COUNTER].name")"
-    #echo $USAGE_URL
+    echo $USAGE_URL
     curl -s -u$BINTRAY_USERNAME:$BINTRAY_APIKEY -X POST "$USAGE_URL" -T timeslice.json > usage.txt
     if cat usage.txt | jq -r '.[0]'; then
-	echo "Repository $(cat repos.txt | jq -r ".[$COUNTER].name"); Storage: $(cat usage.txt | jq -r '.[0].storage_bytes') Downloaded: $(cat usage.txt | jq -r '.[0].download_bytes') in timeslice"
-	echo "$(cat repos.txt | jq -r ".[$COUNTER].name"), $(cat usage.txt | jq -r '.[0].storage_bytes'), $(cat usage.txt | jq -r '.[0].download_bytes')" >> usage.csv
+	DL=$(cat usage.txt | jq -r '.[0].download_bytes')
+    STORAGE=$(cat usage.txt | jq -r '.[0].storage_bytes')
+	echo "Repository $(cat repos.txt | jq -r ".[$COUNTER].name"); Storage: $STORAGE Downloaded: $DL in timeslice"
+	echo "$(cat repos.txt | jq -r ".[$COUNTER].name"), $STORAGE, $DL, $(bc -l <<< $STORAGE/$TOTAL_STORAGE), $(bc -l <<< $DL/$TOTAL_DL)" >> usage.csv
     else
 	echo ERROR: $(cat usage.txt)
     fi
