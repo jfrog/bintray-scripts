4dfcb77660c1c8398f33f5d929bd1e7f6d6ca014
Mark Galpin <markg@jfrog.com>
2016-08-23 10:30:17 -0700

    Cleanup of script for efficiency

--- usage-metrics/total-repositories.sh
+++ usage-metrics/total-repositories.sh
@@ -13,16 +13,18 @@ echo $NUM_REPOS
 echo 'repository, storage(b), downloaded(b), storage %, downloaded %' > usage.csv
 COUNTER=0
 while [ $COUNTER -lt $NUM_REPOS ]; do
-    USAGE_URL="https://api.bintray.com/usage/$BINTRAY_ORG/$(cat repos.txt | jq -r ".[$COUNTER].name")"
+    REPONAME=$(cat repos.txt | jq -r ".[$COUNTER].name")
+    USAGE_URL="https://api.bintray.com/usage/$BINTRAY_ORG/$REPONAME"
     echo $USAGE_URL
     curl -s -u$BINTRAY_USERNAME:$BINTRAY_APIKEY -X POST "$USAGE_URL" -T timeslice.json > usage.txt
     if cat usage.txt | jq -r '.[0]'; then
 	DL=$(cat usage.txt | jq -r '.[0].download_bytes')
     STORAGE=$(cat usage.txt | jq -r '.[0].storage_bytes')
-	echo "Repository $(cat repos.txt | jq -r ".[$COUNTER].name"); Storage: $STORAGE Downloaded: $DL in timeslice"
-	echo "$(cat repos.txt | jq -r ".[$COUNTER].name"), $STORAGE, $DL, $(bc -l <<< $STORAGE/$TOTAL_STORAGE), $(bc -l <<< $DL/$TOTAL_DL)" >> usage.csv
+	#echo "Repository $REPONAME; Storage: $STORAGE Downloaded: $DL in timeslice"
+	echo "$REPONAME, $STORAGE, $DL, $(bc -l <<< $STORAGE/$TOTAL_STORAGE), $(bc -l <<< $DL/$TOTAL_DL)" >> usage.csv
     else
 	echo ERROR: $(cat usage.txt)
     fi
     let COUNTER=COUNTER+1
 done
+echo "Completed output to usage.csv"
